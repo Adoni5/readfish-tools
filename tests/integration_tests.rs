10b1443f112d7ba0829cdb59e4a6205ba35f9495
Adoni5 <roryjmunro1@gmail.com>
2023-07-21 10:37:01 +0100

    Add PAF multiplex test

---
+++ tests/integration_tests.rs
@@ -0,0 +1,17 @@
+use readfish_tools::demultiplex_paf;
+
+// importing the common code for tests.
+mod common;
+
+#[test]
+fn test_region_based_paf_demultiplex() {
+    // using common code.
+    let paf = common::get_test_file("test_paf_barcode05_NA12878.paf");
+    let seq_sum = common::get_test_file("sequencing_summary_PAK09329_1e424e0e.txt")
+        .as_os_str()
+        .to_str()
+        .unwrap()
+        .to_string();
+    let toml_path = common::get_test_file("RAPID_CNS2.toml");
+    demultiplex_paf(toml_path, paf, Some(seq_sum))
+}
